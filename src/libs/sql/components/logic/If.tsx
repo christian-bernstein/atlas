4eee96502c0ccf658e93a77ef043096b5736e578
Christian Bernstein <59013801+christian-bernstein@users.noreply.github.com>
2023-01-18 19:02:03 +0100

    unnamed

--- src/libs/sql/components/logic/If.tsx
+++ src/libs/sql/components/logic/If.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import {BernieComponent} from "../../logic/BernieComponent";
-import {Themeable} from "../../logic/style/Themeable";
-import {Assembly} from "../../logic/assembly/Assembly";
 import {getOr} from "../../logic/Utils";
 
 export type IfProps = {
@@ -13,20 +10,17 @@ export type IfProps = {
     undefinedOnEmpty?: boolean
 }
 
-export class If extends BernieComponent<IfProps, any, any> {
-
-    private loadElement(ifVal?: JSX.Element, ifGen?: () => JSX.Element): JSX.Element | undefined {
-        if (ifVal !== undefined) {
-            return ifVal;
-        }
-        ifGen?.();
+const loadElement = (ifVal?: JSX.Element, ifGen?: () => JSX.Element): JSX.Element | undefined => {
+    if (ifVal !== undefined) {
+        return ifVal;
     }
+    ifGen?.();
+}
 
-    componentRender(p: IfProps, s: any, l: any, t: Themeable.Theme, a: Assembly): JSX.Element | undefined {
-        if (p.condition) {
-            return getOr(this.loadElement(p.ifTrue, p.ifTrueGen), getOr(p.undefinedOnEmpty, false) ? undefined : <></>);
-        } else {
-            return getOr(this.loadElement(p.ifFalse, p.ifFalseGen), getOr(p.undefinedOnEmpty, false) ? undefined : <></>);
-        }
+export const If: React.FC<IfProps> = props => {
+    if (props.condition) {
+        return getOr(loadElement(props.ifTrue, props.ifTrueGen), getOr(props.undefinedOnEmpty, false) ? undefined : <></>) ?? <></>;
+    } else {
+        return getOr(loadElement(props.ifFalse, props.ifFalseGen), getOr(props.undefinedOnEmpty, false) ? undefined : <></>) ?? <></>;
     }
 }
