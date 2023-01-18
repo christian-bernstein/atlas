66a54b0bfef464f83fc0c03b5332d455d7aad2f6
Christian Bernstein <59013801+christian-bernstein@users.noreply.github.com>
2023-01-18 16:31:32 +0100

    unnamed

--- src/libs/sql/components/ho/quickPanel/QuickActionPanel.tsx
+++ src/libs/sql/components/ho/quickPanel/QuickActionPanel.tsx
@@ -37,7 +37,7 @@ import {Cursor} from "../../../logic/style/Cursor";
 import {AF} from "../../logic/ArrayFragment";
 import {SideScroller} from "../../layout/SideScroller";
 import {createMargin} from "../../../logic/style/Margin";
-import {FormDataHub} from "../../../../epicure/components/FormDataHub";
+import {FormDataHub} from "../../../../FormDataHub";
 import {StaticDrawerMenu} from "../../lo/StaticDrawerMenu";
 import {Badge} from "../../lo/Badge";
 import _ from "lodash";
