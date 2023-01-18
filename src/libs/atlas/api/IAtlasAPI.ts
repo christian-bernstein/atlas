1976430fe415b402ce030bbd40b6c9bd07f855f3
Christian Bernstein <59013801+christian-bernstein@users.noreply.github.com>
2023-01-18 17:05:22 +0100

    unnamed

--- src/libs/atlas/api/IAtlasAPI.ts
+++ src/libs/atlas/api/IAtlasAPI.ts
@@ -2,7 +2,7 @@ import {Folder} from "../data/Folder";
 import {AtlasDocument} from "../data/AtlasDocument";
 import {Category} from "../data/Category";
 import {IPredicate} from "./IPredicate";
-import {FormDataHub} from "../../epicure/components/FormDataHub";
+import {FormDataHub} from "../../FormDataHub";
 import {AtlasDB} from "./AtlasDB";
 import {IISOAdapter} from "../iso/IISOAdapter";
 
