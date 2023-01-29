bdef27afa4d50782ebfc88a92a6b507fcdf49665
Christian Bernstein <59013801+christian-bernstein@users.noreply.github.com>
2023-01-29 12:01:36 +0100

    unnamed

--- src/libs/atlas/AtlasUtils.ts
+++ src/libs/atlas/AtlasUtils.ts
@@ -2,6 +2,9 @@ import {Buffer} from "buffer";
 
 export namespace AtlasUtils {
 
+    /**
+     * @deprecated
+     */
     export function dataURItoBlob(dataURI: string) {
         // convert base64 to raw binary data held in a string
         // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
